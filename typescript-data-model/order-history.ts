c94ff7a138829055d43ff5c110aa1784a3b07904
Jorge Diaz <j.diazm757@gmail.com>
2024-11-04 05:18:58 +0000

    Completed typescript-data-model exercise

---
+++ typescript-data-model/order-history.ts
@@ -0,0 +1,59 @@
+const orderHistory: any[] = [
+  {
+    product: 'JavaScript for impatient programers',
+    productInfo: {
+      authors: 'Rauschmayer and Dr Axel',
+      warranty: 'Return window closed on sep 7, 2020',
+    },
+    orderplaced: 'August 4, 2020',
+    totalAmount: '$34.00',
+    shipTo: 'ship toL: JS Masher',
+    orderNumber: '114-3941689-8772232',
+    delivered: 'Delivered August 8, 2020',
+  },
+  {
+    product: 'The Timeless Way of Building',
+    productInfo: {
+      authors: 'Alexander, Christopher',
+      warranty: 'Return window closed on August 19, 2020',
+    },
+    orderplaced: 'August 19, 2020',
+    totalAmount: '$44.53',
+    shipTo: 'ship toL: JS Masher',
+    orderNumber: '113-9984268-1280257',
+    delivered: 'Delivered july 20, 2020',
+  },
+  {
+    product: 'GameCube Controller Adapter.',
+    productInfo: {
+      compatibleWith: 'Nintendo Switch, Nintendo WII U, PC.',
+      warranty: 'Return window closed on August 5, 2020',
+    },
+    orderplaced: 'July 4, 2020',
+    totalAmount: '$17.22',
+    shipTo: 'ship toL: JS Masher',
+    orderNumber: '114-2875557-9059409',
+    delivered: 'Delivered july 7, 2020',
+  },
+  {
+    product1: {
+      product: 'GameCube Controller - Super Smash Bros Edition .',
+    },
+    productInfo: {
+      compatibleWith: 'Nintendo Switch.',
+      warranty: 'Return window closed on August 4, 2020',
+    },
+    product2: {
+      product: 'The Art of Sql',
+    },
+    poductInfo: {
+      authors: 'Faroult, Stephane',
+      warranty: 'Return window closed on August 4, 2020',
+    },
+    orderplaced: 'July 3, 2020',
+    totalAmount: '$138.93',
+    shipTo: 'ship toL: JS Masher',
+    orderNumber: '113-2883177-2648248',
+    delivered: 'Delivered july 5, 2020',
+  },
+];
