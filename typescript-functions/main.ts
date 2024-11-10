ffe9e91a20f5fdd1a615ea1dd33ce3903e00c806
Jorge Diaz <j.diazm757@gmail.com>
2024-11-10 22:13:02 +0000

    Completed ty[escript-functions excersice

--- typescript-functions/main.ts
+++ typescript-functions/main.ts
@@ -4,12 +4,71 @@ function convertMinutesToSeconds(minutes: number): number {
   return result;
 }
 const convertMinToSecResult: number = convertMinutesToSeconds(5);
-console.log(convertMinToSecResult);
+console.log('convertMinToSecResult:', convertMinToSecResult);
 
 //  greet(name)
-function greet(name: string = 'Jorge'): string {
-  const result: string = name;
-  return result;
+function greet(name: string): string {
+  const greeting: string = 'Hey, ' + name;
+  return greeting;
 }
-const greetResult: any = greet;
-console.log(greetResult);
+const greetResult: string = greet('Jorge');
+console.log('greetResult:', greetResult);
+
+//  getArea(width, height)
+const getArea = (width: number, height: number): number => {
+  const area: number = width * height;
+  return area;
+};
+const getAreaResult: number = getArea(17, 42);
+console.log('getAreaResult:', getAreaResult);
+
+//  getFirstPersonName(person)
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+const getFirstName = (person: Person): string => {
+  const firstName: string = person.firstName;
+  return firstName;
+};
+const getFirstNameResult: string = getFirstName({
+  firstName: 'Jorge',
+  lastName: 'Diaz',
+});
+console.log('getFirstNameResult:', getFirstNameResult);
+
+//  getLastElement(array)
+const getLastElement = (array: string[]): string => {
+  const lastIndex: number = array.length - 1;
+  const lastElement: string = array[lastIndex];
+  return lastElement;
+};
+const getLastElementResult: string = getLastElement([
+  'propane',
+  'and',
+  'propane',
+  'accesories',
+]);
+console.log('getLastElementResult:', getLastElementResult);
+
+//  callOtherFunction(otherFunction, params)
+function callOtherFunctions(otherFunction: Function, Params: unknown): any {
+  return otherFunction(Params);
+}
+const callOtherFunctionResult: number = callOtherFunctions(
+  convertMinutesToSeconds,
+  10
+);
+console.log('callOtherFunctionResult: ', callOtherFunctionResult);
+
+//  callOtherFunction(otherFunction, params)
+function callOtherFunction(otherFunction: Function, params: unknown): any {
+  return otherFunction(params);
+}
+const callOtherFunctionResults: number = callOtherFunction(getLastElement, [
+  'propane',
+  'and',
+  'propane',
+  'accesories',
+]);
+console.log('callOtherFunctionResults:', callOtherFunctionResults);
