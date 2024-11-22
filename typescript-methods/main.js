bc87e9b9d2a264b4373bc716b9dd9872f355bf69
Jorge Diaz <j.diazm757@gmail.com>
2024-11-22 02:45:05 +0000

    still working on it

---
+++ typescript-methods/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const heroes = ['Spiderman', 'Deadpool', 'Wolverine', 'IronMan'];
+let randomNumber = Math.random();
+randomNumber = randomNumber * heroes.length;
+const randomIndex = Math.floor(randomNumber);
+console.log('randomIndex', randomIndex);
+const randomHero = heroes[randomIndex];
+console.log('randomHero', randomHero);
